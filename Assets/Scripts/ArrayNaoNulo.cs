4f98e6f5a826b8a59a9590e07394b59c33e8cb28
Ivson Santos <ivson.java@gmail.com>
2019-03-19 18:36:31 -0300

    Testa Array Nulo

---
+++ Assets/Scripts/ArrayNaoNulo.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayNaoNulo : MonoBehaviour
+{
+
+    int[] array = { 1, 2, 3, 4, 5};
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        retornaArray();
+        mudaArray();
+        retornaArray();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void retornaArray()
+    {
+        foreach(int i in array)
+        {
+            print(i);
+        }
+    }
+
+    void mudaArray()
+    {
+        int[] novo = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        this.array = novo;
+    }
+}
